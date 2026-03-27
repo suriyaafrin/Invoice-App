7caa47cddf78fd2aa9d91960caf872401d67acc5
suriyaafrin <suriyaislamafrin@gmail.com>
2026-03-27 21:27:10 +0600

    Update4

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const save = document.querySelector(".save");
 const street=document.getElementById("street");
 const value=document.querySelector(".value");
-save.addEventListener("click", (e) => {
+const form=document.querySelector(".input-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-   e.preventDefault(); // Prevent form from refreshing the page
+  const formData = new FormData(form);
+  const street= FormData.get("street");
+  const data = Object.fromEntries(formData.entries());
 
-    // Capture the data
-    const invoiceData = {
-        billFromStreet: document.getElementById("street").value,
-        // Using querySelectorAll or specific IDs is better since you have duplicate IDs in your HTML
-        billToName: document.querySelectorAll("#street")[1].value, 
-        billToEmail: document.querySelectorAll("#street")[2].value,
-        invoiceDate: document.getElementById("ct2").value,
-        // Add more fields here as needed
-    };
 
-    // Save to LocalStorage
-    localStorage.setItem("invoiceData", JSON.stringify(invoiceData));
+  console.log(data);
 
-    // Redirect
-    window.location.href = "form.html";
 });
